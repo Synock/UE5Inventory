22a44f742d7e616f3276dd655495f3ae5a56f793
Synock <maximilienguislain@gmail.com>
2023-11-09 21:35:11 +0100

    Improvements for random loot and stuff

--- Source/InventoryPlugin/Public/Loot/RandomizedEquipmentPool.h
+++ Source/InventoryPlugin/Public/Loot/RandomizedEquipmentPool.h
@@ -6,27 +6,85 @@
 #include "Engine/DataAsset.h"
 #include "RandomizedEquipmentPool.generated.h"
 
-class UInventoryItemEquipable;
+class UInventoryItemBase;
 
+/// Item and probability
 USTRUCT(BlueprintType)
-struct FItemProbability
+struct FItemProbability: public FTableRowBase
 {
 	GENERATED_BODY()
 
 	UPROPERTY(BlueprintReadWrite, EditAnywhere)
-	UInventoryItemEquipable* Item = nullptr;
+	UInventoryItemBase* Item = nullptr;
 
 	UPROPERTY(BlueprintReadWrite, EditAnywhere)
 	float Probability = 1.f;
 
 };
+
+/// A list of possible equipment items, with only one that can be obtained at a time
+USTRUCT(BlueprintType)
+struct FItemAlternative: public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TArray<FItemProbability> AlternativeList;
+
+	// Global probability of this alternative to exist.
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float ExistenceProbability = 1.f;
+
+	UInventoryItemBase* GetItem() const
+	{
+		float TotalProbability = 0.f;
+
+		for (auto& EquipmentData : AlternativeList)
+		{
+			TotalProbability += EquipmentData.Probability;
+		}
+
+		if (TotalProbability > 0.f)
+		{
+			float ProbaStatus = FMath::RandRange(0.f, 1.f);
+			float SumProbability = 0.f;
+			for (const auto& [Item, Probability] : AlternativeList)
+			{
+				const float AlternativeProbability = SumProbability + (Probability / TotalProbability);
+				if (ProbaStatus <= AlternativeProbability)
+				{
+					return Item;
+				}
+
+				SumProbability += AlternativeProbability;
+			}
+		}
+
+		return nullptr;
+	}
+
+};
+
 UCLASS()
-class INVENTORYPLUGIN_API URandomizedEquipmentPool : public UPrimaryDataAsset
+class INVENTORYPLUGIN_API URandomizedLootPool : public UPrimaryDataAsset
 {
 	GENERATED_BODY()
 
 public:
 
 	UPROPERTY(BlueprintReadWrite, EditAnywhere)
-	TArray<FItemProbability> EquipmentSet;
+	float MinCoin = 0.f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float MaxCoin = 0.f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float ProbabilityCoin = 1.f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TArray<FItemProbability> LootPool;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TArray<FItemAlternative> AlternativeSet;
 };
+
